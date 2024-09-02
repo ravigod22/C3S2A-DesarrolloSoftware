a3c1e5b335e4fcad119351e3fcebf74083fc7b31
ravigod22 <ravi22135@gmail.com>
2024-09-02 14:12:13 -0500

    Segunda Version (Optimizacion)

--- Pregunta1/DijsktraOptimization.cpp
+++ Pregunta1/DijsktraOptimization.cpp
@@ -14,22 +14,35 @@ vector<pair<int, int>> Adj[maxnodes];
 // Algoritmo de dijsktra en tiempo O(nlogn + mlogn);0
 // donde n = # nodes, m = # edges
 vector<int> DijsktraOptimization(int source) {
+    // Inicializamos un contenedor con las distancias calculadas
     vector<int> dis(nodes + 1, INF);
+
+    // Inicializamos el nodo arbitrario
     dis[source] = 0;
     
+    // Utilizamos de un contenedor llamado cola de prioridad para realizar la optimizacion
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq_min;
+
+    // Introduccimos el nodo arbitrario
     pq_min.emplace(dis[source], source);
 
+    // Mientras exista posibilidades
     while (!pq_min.empty()) {
+        // Accedemos al elemento con distancia minima
         int u = pq_min.top().second;
         int w = pq_min.top().first;
 
+        // Verificacion de si existe un nodo anterior que fue
+        // visitado y verificar si la distancia diferente
         if (dis[u] != w) continue;
 
+        // Visitamos los posibles caminos gracias a sus vecinos
         for (auto e : Adj[u]) {
             int to = e.first;
             int len = e.second;
-
+            
+            // Actualizamos si es posible llegar a un nodo vecino
+            // con una distancia menor a la calculada
             if (dis[u] + len < dis[to]) {
                 dis[to] = dis[u] + len;
                 pq_min.emplace(dis[to], to);
@@ -48,7 +61,7 @@ int main() {
     // Realizaremos el algoritmo en tiempo O(nlogn + mlogn)
     // Aqui denotaremos dos estructuras para mejorar el tiempo
     // Utilizar un set, conjunto de arboles o,
-    // Utilizar una cola de prioridad minima de heap
+    // Utilizar una cola de prioridad minima de una estructura heap.
     vector<int> OptimizationVersion = DijsktraOptimization(0);    
     return 0;
 }

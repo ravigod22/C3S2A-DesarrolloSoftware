90038c4af61a901dc6ca20053002ccce6cb4ba88
ravigod22 <ravi22135@gmail.com>
2024-09-04 08:30:56 -0500

    Second Commit

---
+++ Actividad1/devops-practice/src/server.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port ${server.address().port}');
+});
+
+
+module.exports = serve;
